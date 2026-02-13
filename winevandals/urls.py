2144c1ef6b27785096d5faa18c5f0af33182d292
Griftersbar <steve@elevate3dservices.com>
2026-02-13 17:17:49 +0000

    Print ALLOWED_HOSTS from settings
    Added a print statement to display ALLOWED_HOSTS.

--- winevandals/urls.py
+++ winevandals/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+print("ALLOWED_HOSTS:", settings.ALLOWED_HOSTS)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
